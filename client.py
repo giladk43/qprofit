0312b5e7bba4436d99f0bbf8c3efc2add31d30fa
Gilad Kogan <kogangilad@gmail.com>
2025-06-25 13:05:21 +0300

    Server is mostly done

--- client.py
+++ client.py
@@ -5,21 +5,23 @@ import argparse
 
 BUFFER = 1024
 
-def get_address():
+def get_args():
     """
     Gets the ip and port from the cmd
     """
     parser = argparse.ArgumentParser(description='What the program does')
-    parser.add_argument('ip', help='The ip of the server address', type= str)
+    parser.add_argument('ip', help='The ip of the server address', type=str)
     parser.add_argument('port', help='The port of the server address', type=int)
+    parser.add_argument('name', help='The name of the client', type=str)
+    parser.add_argument('room_name', help='The room name', type=str)
     args = parser.parse_args()
     return args
     
 
 def handle_client():
     my_sock = socket.socket()
-    address = get_address()
-    my_sock.connect(address)
+    args = get_args()
+    my_sock.connect((args.ip, args.port))
     
 
 
--- server.py
+++ server.py
@@ -7,6 +7,7 @@ from collections import defaultdict
 
 BUFFER = 1024
 TIMEOUT = 1
+FINISH = "/exit"
 
 def get_address():
     """
@@ -18,27 +19,52 @@ def get_address():
     args = parser.parse_args()
     return args
 
+def connect_server(server_sock, address):
+    try:
+        server_sock.bind(address)
+        server_sock.listen(1)
+        server_sock.setblocking(False)
+    except socket.gaierror:
+        print("This is not a valid address")
 
-def handle_rooms():
+def handle_rooms(session, data, client_list):
+    for client_session in client_list:
+        if session != client_session:
+            client_session.send(data.encode())
     
+    
+def get_room(session, clients_in_rooms):
+    for room, client_list in clients_in_rooms:
+        if session in client_list:
+            return room
 
-
-def handle_clients(session_list):
+def handle_clients(session_list, clients_in_rooms):
     readable, _, _ = select.select(session_list, [], []) 
     
     for session in readable:
-        if 
-
+        room_name = get_room(session, clients_in_rooms)
+        recv_data = session.recv(BUFFER).decode()
+        if recv_data == FINISH:
+            session_list.remove(session)
+            clients_in_rooms[room_name].remove(session)
+            session.close()
+        else:
+            handle_rooms(session, data, clients_in_rooms[room_name])
+            
+        
 def handle_server():
     session_list = []
-    clients_in_rooms = defaultdict(list)
+    first = True
+    clients_in_rooms = defaultdict(list) #Will hold a dict with each session in a room
     address = get_address()
     server_sock = socket.socket()
-    server_sock.bind(address)
-    server_sock.listen(1)
-    while session_list != []:
-        conn, addr = server_socket.accpet()
+    handle_server(server_sock, address)
+    while session_list != [] or first:
+        if first:
+            first = False
+        conn, addr = server_socket.accept()
         session_list.append(conn)
+        clients_in_rooms["test"].append(session) #Need to receive the room 
         handle_clients(session_list)
     
     server_sock.close()
@@ -49,3 +75,8 @@ def main():
     
 if __name__ == "__main__":
     main()
+
+
+#functions that still need to be added, transfer -> Use default dict and remove to change where it is
+#Get room name from client at the start
+#admin
